7f04ae7583456f91d1b22f506363ee7435434169
abhitachi <abhisekshetter1999@gmail.com>
2022-11-12 23:14:05 +0530

    added style state and events

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import React from "react";
-import Board from "./Components/Board";
+import React from 'react';
+import Board from './Components/Board';
+
+import './styles/root.scss';
 
 const App = () => {
   return (
-    <div>
+    <div className="app">
       <h1>TIC TACK TOE</h1>
       <Board />
     </div>
--- src/Components/Square.jsx
+++ src/Components/Square.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React from 'react';
 
-const Square = ({ value }) => {
+const Square = ({ value, onClick }) => {
   //   console.log(props);
-  return <button type="button">{value}</button>;
+  return (
+    <button type="button" className="square" onClick={onClick}>
+      {value}
+    </button>
+  );
 };
 
 export default Square;
